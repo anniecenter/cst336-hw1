6510446ab41d7a1c2a6d18e5e28c44230545897b
EC2 Default User <ec2-user@ip-172-31-13-23.us-west-1.compute.internal>
2020-11-04 06:52:31 +0000

    Switched to ordered list

--- index.php
+++ index.php
@@ -19,12 +19,12 @@
                 <h2>Introduction</h2>
                 <p>A <strong>programming language</strong> is a set of high-level instructions that programmers can use to tell a computer what to do.</p>
                 <p>Today, there are hundreds of different programming languages to choose from, but four of the most popular are:</p>
-                <ul>
+                <ol type='1'>
                     <li>Java</li>
                     <li>C++</li>
                     <li>C#</li>
                     <li>Python</li>
-                </ul>
+                </ol>
                 <p>Click the links in the navigation bar at the top of the screen to learn more about each programming language.</p>    
             </div>
         </article>
